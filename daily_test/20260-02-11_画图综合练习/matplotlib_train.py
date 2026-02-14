a6b96137de4df5d0e9d858fffe2049c665890728
luanqiushi1211 <823032764@qq.com>
2026-02-14 12:48:58 +0800

    画图综合练习修改
    画图综合练习修改

--- daily_test/20260-02-11_画图综合练习/matplotlib_train.py
+++ daily_test/20260-02-11_画图综合练习/matplotlib_train.py
@@ -5,6 +5,7 @@
 4、绘制向量的三种范数的等高线图
 """
 from ast import List, Tuple
+from turtle import color
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -30,33 +31,63 @@ def least_squares(point_set:List):
         res = inverse_gram_matrix @ cofficient_matrix.T @ res_vector
         c,d = res
         print(f"最优解的偏移量= {d},斜率={c}")
-        draw_picture(point_set,(d,c))
+        fig = draw_picture(point_set,(d,c))
+
+
 
 
 def draw_picture(point_set:list,line_set:Tuple):
     c,d = line_set
-    fig = plt.figure(figsize=(10,10))
-    ax = fig.add_subplot(111)
+    fig = plt.figure(figsize=(20,8))
+    ax = fig.add_subplot(121)
     x_space = np.arange(-10,10,1)
     y_space = np.arange(-10,10,1) 
     X,Y = np.meshgrid(x_space,y_space)
     res_space = d * np.array(x_space)+c
-    for i in range(0,len(y_space)):
-       
-        ax.plot(X[i,:],Y[i,:],"gray")
-    for k in range(0,len(x_space)):
-       ax.plot(X[:,k],Y[:,k],"gray")          
+    ax.grid(True,color = "gray",linestyle = '-',alpha = 0.3)
+      
     
     ax.plot(x_space,res_space,"red")
     for point_tuple in point_set:
         x,y = point_tuple
         ax.scatter(x,y,c="blue")
     plt.show()
+    return fig
         
 
- 
+def is_positive(matrix:np.array):
+    eigenvalues,eigenvectors = np.linalg.eig(matrix)
+    if np.all(eigenvalues>0):
+        print(f"{matrix}是正定矩阵")
+    x_data_set = np.linspace(-10,10,100)
+    y_data_set = np.linspace(-10,10,100)
+    z_data_set = []
+    X,Y = np.meshgrid(x_data_set,y_data_set)
+    for i in x_data_set:
+        for j in y_data_set:
+            res = np.array([[i,j]]) @ matrix @ np.array([[i,j]]).T
+            z_data_set.append(res)
+    Z = np.reshape(z_data_set,np.shape(X))
+    fig = plt.figure(figsize=(15,15))
+    ax = fig.add_subplot(121,projection = "3d")
+    ax.set_title("二次型3D曲面图")
+    ax.set_label("X")
+    ax.set_label("Y")
+    ax.set_label("Z")
+    surf = ax.plot_surface(X,Y,Z,cmap = 'viridis',rstride = 5)
+    fig.colorbar(surf)   
+    ax2 = fig.add_subplot(122)
+    ax2.set_xlim(-2,2)
+    ax2.set_ylim(-2,2)
+    ax2.grid(True,color = 'b',linestyle = '-',linewidth = 2)
+    for i in eigenvectors:
+        x,y = i
+        ax2.quiver(0,0,x,y,angles = 'xy',scale_units='xy',scale = 1,headwidth=4,  headlength=5, headaxislength=4.5)
+    plt.show()
+
 
 
 if __name__ == "__main__":
     point_set = [(1,2),(2,4),(3,7)]
-    least_squares(point_set)
+    matrix = np.array([[1,2],[2,1]])
+    is_positive(matrix)
