acbd7f71bc56de4ca9a83451295981e680983c8f
luanqiushi1211 <823032764@qq.com>
2026-03-04 22:58:08 +0800

    修改cholesky分解算法
    修改cholesky分解算法

--- daily_test/2026_02_28_Cholesky分解/cholesky_seperator.py
+++ daily_test/2026_02_28_Cholesky分解/cholesky_seperator.py
@@ -1,25 +1,27 @@
 import numpy as np
 def cholesky(matrix:np.array)->np.array:    
     row,cloum = np.shape(matrix)
-    L = np.eye(row,cloum)
-    sum1 = 0
-    for i1 in range(0,cloum):
-        L[i1][i1] = np.sqrt(matrix[i1][i1]-sum1)
-        sum1 = 0
+    L = np.zeros((row,cloum))
+    sum = 0
+    for i1 in range(0,row):
         for i2 in range(0,i1+1):
-            if i1 == cloum-1:
-                break
-            sum2 = 0
-            for i3 in range(0,i2-1):
-                sum2 = sum2+L[i1+1][i3]*L[i2][i3]
-            L[i1+1][i2] = (matrix[i1+1][i2]-sum2)/L[i2][i2]
-            sum1 = sum1+np.square(L[i1+1][i2])
+            sum=0
+            for i3 in range(0,i2):
+                sum = sum+L[i1][i3]*L[i2][i3]
+            if i1!=i2:
+                L[i1][i2] = (matrix[i1][i2]-sum)/L[i2][i2]
+            else:
+                cha = matrix[i1][i2]-sum
+                L[i1][i2] = np.sqrt(cha)
+   
     return L
 
 if __name__ == "__main__":
     matrix = np.array([[4,2,2],[2,5,3],[2,3,6]])
     L_matrix = cholesky(matrix)
+    L_matrix_np = np.linalg.cholesky(matrix)
     print(f"f原矩阵：{matrix}的Cholesky分解矩阵是：{L_matrix}")
+    print(f"f原矩阵：{matrix}的使用numpy库方法的Cholesky分解矩阵是：{L_matrix_np}")
 
 
     
