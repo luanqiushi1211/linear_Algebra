a5f9583d922884a5da84617735eb123c20c38c1b
luanqiushi1211 <823032764@qq.com>
2026-03-09 23:24:32 +0800

    QR分解和求逆法求最小二乘最优解对比
    QR分解和求逆法求最小二乘最优解对比

---
+++ daily_test/20260_03_07QR分解/QRLeastSquares.py
@@ -0,0 +1,22 @@
+import numpy as np
+import scipy.linalg as sl
+
+def QRLeastSquares():
+    sick_matrix = sl.hilbert(15)
+    a_0 = np.ones(15)
+    y = sick_matrix @ a_0
+    Q,R = np.linalg.qr(sick_matrix)
+    a_1 = np.linalg.inv(sick_matrix.T @ sick_matrix)@sick_matrix.T@y
+    a_2 = sl.solve_triangular(R,Q.T@y)
+
+    deviation1 = np.linalg.norm(a_1-a_0)
+    deviation2 = np.linalg.norm(a_2-a_0)
+
+    print(f"希尔伯特矩阵是：{sick_matrix}")
+    print(f"最小二乘结果向量是：{a_0}")
+    print(f"Q,R分别是：{Q},{R}")
+    print(f"求逆方法得到的最优解是：{a_1}，误差是：{deviation1}")
+    print(f"QR分解方法得到的最优解是：{a_2}，误差是：{deviation2}")
+
+if __name__ == "__main__":
+    QRLeastSquares()
